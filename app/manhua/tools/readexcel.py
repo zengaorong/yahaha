a214b09882508fd833e675a9036a76f04b2e3087
leo <zengaorong@qq.com>
2018-10-22 06:58:38 +0800

    book 页面更新

--- app/manhua/tools/readexcel.py
+++ app/manhua/tools/readexcel.py
@@ -1,38 +1,54 @@
+# -*- coding: utf-8 -*-
 import xlrd
+import xlwt
+from datetime import date,datetime
+from xlutils.copy import copy
 
-def readexcel_todict(addr_str,key_num,begin_num,sheetnum):
-    excel_workbook = xlrd.open_workbook(addr_str)
-    excel_sheet01 = excel_workbook.sheets()[sheetnum]
-    terminal_num_nrows = excel_sheet01.nrows
-    terminal_num_ncols = excel_sheet01.ncols
-    excel_dict =  {}
-    for r in range(begin_num,terminal_num_nrows):
-        key_str = excel_sheet01.cell(r,key_num).value
-        if type(key_str) is float:
-            key_str = str(int(key_str))
-        if check_is_nums(key_str):
-            #print key_str + "123"
-            key_str = int(key_str)
-        hold = []
-        #print int(key_str)
-        for c in range(0,terminal_num_ncols):
-            if type(excel_sheet01.cell(r,c).value) is float:
+def read_excel():
+    # # 打开文件
+    # workbook = xlrd.open_workbook(r'books.xls')
+    # # 获取所有sheet
+    # print workbook.sheet_names() # [u'sheet1', u'sheet2']
+    # sheet2_name = workbook.sheet_names()[1]
+    #
+    # # 根据sheet索引或者名称获取sheet内容
+    # sheet2 = workbook.sheet_by_index(1) # sheet索引从0开始
+    # sheet2 = workbook.sheet_by_name('Sheet2')
+    #
+    # # sheet的名称，行数，列数
+    # print sheet2.name,sheet2.nrows,sheet2.ncols
+    #
+    # # 获取整行和整列的值（数组）
+    # rows = sheet2.row_values(3) # 获取第四行内容
+    # cols = sheet2.col_values(2) # 获取第三列内容
+    # print rows
+    # print cols
+    #
+    # # 获取单元格内容
+    # print sheet2.cell(1,0).value.encode('utf-8')
+    # print sheet2.cell_value(1,0).encode('utf-8')
+    # print sheet2.row(1)[0].value.encode('utf-8')
+    #
+    #
+    # print xlrd.xldate_as_tuple(sheet2.cell_value(2,2),workbook.datemode)
+    # date_value = xlrd.xldate_as_tuple(sheet2.cell_value(2,2),workbook.datemode)
+    # print date(*date_value[:3]).strftime('%Y/%m/%d')
+    # # 获取单元格内容的数据类型
+    # print sheet2.cell(2,2).ctype
 
-                hold.append(str(int(excel_sheet01.cell(r,c).value)))
-            else:
-                hold.append(excel_sheet01.cell(r,c).value)
-        excel_dict[key_str] =  hold
+    workbook = xlrd.open_workbook(r'books.xls')
+    sheet2 = workbook.sheet_by_name('account')
+    sheet = workbook.sheet_by_name(sheet2.cell(1,0).value)
+    print sheet.cell(1,0)
+    print sheet.cell(1,1)
 
-    return excel_dict
-def check_is_nums(str_check):
-    isnum = True
-    for key in str_check.decode('utf-8'):
-        if u'\u0030'<=key<=u'\u0039':
-            pass
-        else:
-            isnum = False
-            return isnum
+    newbook = copy(workbook)
+    new_sheet = newbook.get_sheet(2)
+    #sheet_change = workbook.sheet_by_name(sheet2.cell(1,0).value)
+    new_sheet.write(2,1,"123")
+    newbook.save(r'books.xls')
 
-    if str_check == "":
-        isnum = False
-    return isnum
+
+
+if __name__ == '__main__':
+    read_excel()
