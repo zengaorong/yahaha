f2577c03a144ad9628da3df781222f0bbe680381
leo <zengaorong@qq.com>
2018-11-18 20:05:36 +0800

    添加时间控件 更正数据库错误 添加列表

--- flasky.py
+++ flasky.py
@@ -22,5 +22,5 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-app.run(host='127.0.0.1',port=8083,debug=True)
+#app.run(host='127.0.0.1',port=8083,debug=True)
 app.run(host='192.168.8.129',port=8083,debug=True)
